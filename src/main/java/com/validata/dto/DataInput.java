f4d5ea802b9d84592fac6909e9c676da02916d6e
Wallisson Stevan <162034611+wstvns@users.noreply.github.com>
2026-01-08 23:36:37 -0300

    feat: Create DataInput class for data encapsulation

---
+++ src/main/java/com/validata/dto/DataInput.java
@@ -0,0 +1,15 @@
+package com.validata.dto;
+
+public class DataInput {
+
+    private final String value;
+
+    public DataInput(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+}
